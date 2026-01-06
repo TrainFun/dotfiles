3ef918ab9cd19a8d9e9417208ccc898c005a203c
TrainFun <39642409+TrainFun@users.noreply.github.com>
2026-01-06 14:34:35 +0800

    nvim: cb = unnamedplus

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,8 +1,8 @@
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = function(_, opts)
-      return {}
-    end,
-  },
+  -- {
+  --   "MeanderingProgrammer/render-markdown.nvim",
+  --   opts = function(_, opts)
+  --     return {}
+  --   end,
+  -- },
 }
